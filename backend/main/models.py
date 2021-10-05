386fd1d073335bacbe051fc2d42c31776e3cb945
kunal <kunalbhatia868@gmail.com>
2021-10-06 01:04:00 +0530

    added models and serializers

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    content = models.CharField(max_length=256)
+    created_on = models.DateTimeField(timezone.now())
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.content
+
+
+class Choice(models.Model):
+    content = models.CharField(max_length=100)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
---
+++ backend/main/serializers.py
@@ -0,0 +1,25 @@
+from django.db.models import fields
+from rest_framework import serializers
+from . import models
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Tag
+        fields = '__all__'
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(read_onlu=True, many=True)
+
+    class Meta:
+        model = models.Question
+        fields = '__all__'
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    question = QuestionSerializer(read_only=True)
+
+    class Meta:
+        model = models.Choice
+        fields = '__all__'
