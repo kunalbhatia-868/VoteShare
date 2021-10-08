27b0e818ae9fa1fa38da14b3fd2bbcc587e31fcd
kunal <kunalbhatia868@gmail.com>
2021-10-08 14:02:51 +0530

    small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import HeroSection from "./components/HeroSection/HeroSection";
 import NavBar from "./components/Navbar/NavBar.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import TopPolls from "./components/TopPolls/TopPolls";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
